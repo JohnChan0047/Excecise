17ee54352ab3238e714eb1f27b2f20ecd774930b
chenrb <410618634@qq.com>
2018-03-07 22:24:52 +0800

    刷题宝

---
+++ 刷题宝.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*- 
+__author__ = 'John 2018/3/7 21:35'
+
+print(set([1, 2]))  # {1，2}
+print((2))  # 2
+print((2,))  # （2，）
+print("g".center(5, "o"))  # 'oogoo'
+print("google".count("o", 0, 2))  # 1
+print("usa13".isalnum())  # True
