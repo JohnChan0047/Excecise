e2231c9633a61f74c7e3acb30849dd0e6e763900
chenrb <410618634@qq.com>
2018-03-08 19:42:00 +0800

    刷题

---
+++ 刷题宝(Python基础).py
@@ -0,0 +1,84 @@
+# -*- coding:utf-8 -*- 
+__author__ = 'John 2018/3/7 21:35'
+import string
+
+print(set([1, 2]))  # {1，2}
+print((2))  # 2
+print((2,))  # （2，）
+print("g".center(5, "o"))  # 'oogoo'
+print("google".count("o", 0, 2))  # 1
+print("Usa13".isalnum())  # True 判断是否字母或数字
+print("".isspace())  # False 判断是否空格
+print(','.join(['a', 'b']))  # 'a,b'
+print('www.example.com'.lstrip('cmowz.'))  # 'example.com'
+print('aBc'.swapcase())  # 'AbC'
+print('5'.zfill(10))  # 0000000005字符串前添加0
+print(string.digits)
+
+#
+a = 1
+
+
+def do_something():
+    global a  # 在函数内部给全局变量赋值
+    a = 2
+
+
+do_something()
+print(a)
+#
+
+
+#
+def minus(a, b):
+    x = a - b
+
+
+print(minus(b=5, a=4))
+#
+
+
+#
+params1 = (8, 9)
+params2 = {"a": 4, "b": 5}
+
+
+def minus(a, b):
+    return a - b
+
+
+print("%d,%d" % (minus(*params1), minus(**params2)))
+#
+
+
+#
+class A(object):
+    attr = 4
+
+
+class B(A):
+    def __init__(self, b):
+        self.x = b
+
+
+class C(B):
+    x = 6
+
+
+print(C(9).x)
+#
+
+
+# 静态方法
+class Ball(object):
+
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def func(x, y):
+        return x * y
+
+
+print(Ball.func(2, 6))
+#
--- 刷题宝.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*- 
-__author__ = 'John 2018/3/7 21:35'
-
-print(set([1, 2]))  # {1，2}
-print((2))  # 2
-print((2,))  # （2，）
-print("g".center(5, "o"))  # 'oogoo'
-print("google".count("o", 0, 2))  # 1
-print("usa13".isalnum())  # True
