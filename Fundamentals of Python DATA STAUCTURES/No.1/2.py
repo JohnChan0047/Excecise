e0b23b3ba24bf88603138e240b36f70b5d660cf6
chenrb <rubi@canway.net>
2019-03-31 14:35:34 +0800

    no1.2

--- Fundamentals of Python DATA STAUCTURES/No.1/2.py
+++ Fundamentals of Python DATA STAUCTURES/No.1/2.py
@@ -1,1 +1,26 @@
 # -*- coding: utf-8 -*-
+
+
+def weekly_pay(hour_pay, routine_hours, out_time):
+    """
+    雇员周薪
+    :param hour_pay: 日薪
+    :param routine_hours: 每周常规工作时间
+    :param out_time: 加班时间
+    :return: 周薪
+    """
+    return hour_pay * routine_hours + 1.5 * hour_pay * out_time
+
+
+if __name__ == '__main__':
+    hour_pay, routin_hours, out_time = 0, 0, 0
+    while True:
+        try:
+            hour_pay = float(input("请输入日新："))
+            routin_hours = float(input("请输入每周常规工作时间："))
+            out_time = float(input("请输入每周加班时间: "))
+            break
+        except ValueError:
+            print("输入出错，请输入数字！")
+
+    print("周薪：{}".format(weekly_pay(hour_pay, routin_hours, out_time)))
